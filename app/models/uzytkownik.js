4283c6548426a4b88853384670bf19faaf35b715
PawelSzy <peal1982@gmail.com>
2017-01-19 22:45:18 +0100

    Testowanie wagner inwoke uzytkownicy

--- app/models/uzytkownik.js
+++ app/models/uzytkownik.js
@@ -2,6 +2,23 @@ var mongoose = require('mongoose');
 var ObjectId = require('mongodb').ObjectID;
 // mongoose.Types.ObjectId("<object_id>");
 // require('mongoose').Schema.ObjectId ;
-var uzytkownikSchema = require('../../config/uzytkownikSchema');
+// var uzytkownikSchema = require('../../config/uzytkownikSchema');
 
-module.exports = mongoose.model('Uzytkownik', uzytkownikSchema);
+// module.exports = mongoose.model('Uzytkownik', uzytkownikSchema);
+
+
+// Wagner
+
+module.exports = function(wagner) {
+	 // mongoose.connect('mongodb://localhost/uzytkownik');
+
+	var uzytkownikSchema = require('../../config/uzytkownikSchema');
+	var Uzytkownicy = mongoose.model('Uzytkownik', uzytkownikSchema);
+
+	wagner.factory('Uzytkownicy', function() {
+		return Uzytkownicy;
+	});
+
+	return { Uzytkownicy: Uzytkownicy};
+
+};
